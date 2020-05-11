4ac7545dddd0db2130660d5f7175612b3e827630
israel chavez <izrra.ch@icloud.com>
2020-05-10 22:45:28 -0700

    +Diagnostics task meassurer +Tools task parallel runner

---
+++ Combine.Sdk/Diagnostics/Performance/Task.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Combine.Sdk.Diagnostics.Performance
+{
+  /// <summary>
+  /// Provides a mechanism to measure tasks performance
+  /// </summary>
+  public class TaskMeasure
+  {
+    /// <summary>
+    /// Creates a new task performance instance
+    /// </summary>
+    public TaskMeasure()
+    {
+
+    }
+
+    /// <summary>
+    /// Measures the time that takes each
+    /// task to complte
+    /// </summary>
+    /// <typeparam name="T">Type of task</typeparam>
+    /// <param name="tasks">Task array</param>
+    /// <returns>List of key value task</returns>
+    public List<KeyValuePair<Task<T>, TimeSpan>> Measure<T>(Task<T>[] tasks)
+    {
+      Stopwatch stopwatch = new Stopwatch();
+      List<KeyValuePair<Task<T>, TimeSpan>> times = new List<KeyValuePair<Task<T>, TimeSpan>>();
+      stopwatch.Start();
+      while (tasks.Length > 0)
+      {
+        int index = Task.WaitAny(tasks);
+        Task<T> completed = tasks[index];
+        times.Add(new KeyValuePair<Task<T>, TimeSpan>(completed, stopwatch.Elapsed));
+        List<Task<T>> temp = tasks
+          .ToList();
+        temp.RemoveAt(index);
+        tasks = temp.ToArray();
+      }
+      stopwatch.Stop();
+      return times;
+    }
+
+    /// <summary>
+    /// Measures the time that takes each
+    /// task to complte in parallel
+    /// </summary>
+    /// <typeparam name="T">Type of task</typeparam>
+    /// <param name="tasks">Task array</param>
+    /// <returns>List of key value task</returns>
+    public List<KeyValuePair<Task<T>, KeyValuePair<int, TimeSpan>>> MeasureInParallel<T>(Task<T>[] tasks)
+    {
+      List<KeyValuePair<Task<T>, KeyValuePair<int, TimeSpan>>> times = new List<KeyValuePair<Task<T>, KeyValuePair<int, TimeSpan>>>();
+      Stopwatch stopwatch = new Stopwatch();
+      stopwatch.Start();
+      Parallel.ForEach(tasks, task =>
+      {
+        task.Wait();
+        times.Add(new KeyValuePair<Task<T>, KeyValuePair<int, TimeSpan>>(task, new KeyValuePair<int, TimeSpan>(Thread.CurrentThread.ManagedThreadId, stopwatch.Elapsed)));
+      });
+      stopwatch.Stop();
+      return times;
+    }
+  }
+}
---
+++ Combine.Sdk/Tools/Tasks/ParallelRunner.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Combine.Sdk.Tools.Tasks
+{
+  /// <summary>
+  /// Provides a mechanism to runs tasks
+  /// in parallel
+  /// </summary>
+  public class ParallelRunner
+  {
+    /// <summary>
+    /// Creates a new runner task instance
+    /// </summary>
+    public ParallelRunner()
+    {
+
+    }
+
+    /// <summary>
+    /// Runs all the specified tasks in parallel
+    /// in a non blocking thread way
+    /// </summary>
+    /// <typeparam name="T">Task result type</typeparam>
+    /// <param name="tasks">Array of task</param>
+    /// <returns>Task State</returns>
+    public async Task Run<T>(Task<T>[] tasks)
+    => await Task.Run(() => Parallel.ForEach(tasks, task => task.Wait()));
+
+    /// <summary>
+    /// Runs all the specified tasks in parallel
+    /// and waits until all are finish
+    /// </summary>
+    /// <typeparam name="T">Task result type</typeparam>
+    /// <param name="tasks">Array of task</param>
+    public void RunAndWait<T>(Task<T>[] tasks)
+    => Parallel.ForEach(tasks, task => task.Wait());
+
+    /// <summary>
+    /// Runs all the specified tasks in parallel
+    /// in a non blocking thread way
+    /// </summary>
+    /// <typeparam name="T">Task result type</typeparam>
+    /// <param name="tasks">Task list</param>
+    /// <returns>Task State</returns>
+    public async Task Run<T>(List<Task<T>> tasks)
+    => await Task.Run(() => Parallel.ForEach(tasks, task => task.Wait()));
+
+    /// <summary>
+    /// Runs all the specified tasks in parallel
+    /// and waits until all are finish
+    /// </summary>
+    /// <typeparam name="T">Task result type</typeparam>
+    /// <param name="tasks">Task list</param>
+    public void RunAndWait<T>(List<Task<T>> tasks)
+    => Parallel.ForEach(tasks, task => task.Wait());
+  }
+}
