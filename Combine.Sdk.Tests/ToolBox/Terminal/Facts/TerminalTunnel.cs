ded05b5b81217dd8187b5200e034463408d73a88
israel chavez <izrra.ch@icloud.com>
2020-06-21 21:30:04 -0700

    #ToolBox +TerminalTunnel

---
+++ Combine.Sdk.Tests/ToolBox/Terminal/Facts/TerminalTunnel.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using System.Text;
+using System.Threading.Tasks;
+using Combine.Sdk.ToolBox.Terminal;
+using Combine.Sdk.Data.Definitions.Response;
+
+namespace Combine.Sdk.Tests.ToolBox.Terminal.Facts
+{
+  /// <summary>
+  /// Provides a mechanism to test all the methods
+  /// in the TerminalTunnel instance available
+  /// </summary>
+  public class TerminalTunnelTests
+  {
+    /// <summary>
+    /// Creates a new terminal tunnel test instance
+    /// </summary>
+    public TerminalTunnelTests()
+    {
+
+    }
+
+    /// <summary>
+    /// Proves that the Run method
+    /// runs the specified command
+    /// through the terminal tunnel
+    /// instance and collects the
+    /// redirected output stream
+    /// </summary>
+    /// <returns>Task State</returns>
+    [Fact]
+    public async Task Run()
+    {
+      TerminalTunnel tunnel = new TerminalTunnel();
+      ComplexResponse<StringBuilder> output = await tunnel.Run(@"wmic", @"cpu get name /Value");
+      Assert.True(output.Correct);
+    }
+  }
+}
---
+++ Combine.Sdk/ToolBox/Terminal/TerminalTunnel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Combine.Sdk.Extensions.CommonObjects;
+using Combine.Sdk.Data.Definitions.Response;
+
+namespace Combine.Sdk.ToolBox.Terminal
+{
+  /// <summary>
+  /// Provides a mechanism to run command prompt/terminal commands
+  /// through a non ui visible tunnel
+  /// </summary>
+  public class TerminalTunnel
+  {
+    /// <summary>
+    /// Creates a new terminal tunnel instance
+    /// </summary>
+    public TerminalTunnel()
+    {
+
+    }
+
+    /// <summary>
+    /// Runs the specified command and
+    /// collects the output response
+    /// </summary>
+    /// <param name="fileName">File, program, process</param>
+    /// <param name="arguments">Command arguments</param>
+    /// <returns>String output</returns>
+    public async Task<ComplexResponse<StringBuilder>> Run(string fileName = @"cmd.exe", string arguments)
+    {
+      //Verify executable and command args
+      if (fileName.IsNotValid() || arguments.IsNotValid())
+        return new ComplexResponse<StringBuilder>(false, @"The supplied filename/arguments are not valid to work with.");
+      ComplexResponse<StringBuilder> response;
+      try
+      {
+        //Create process reference
+        ProcessStartInfo info = new ProcessStartInfo
+        {
+          FileName = fileName,
+          Arguments = arguments,
+          RedirectStandardOutput = true
+        };
+        //Invoke process
+        using (Process process = Process.Start(info))
+        {
+          StringBuilder sb = new StringBuilder();
+          //Read redirected output stream
+          while (!process.StandardOutput.EndOfStream)
+            sb.AppendLine(await process.StandardOutput.ReadLineAsync());
+          response = new ComplexResponse<StringBuilder>(sb);
+          process.Dispose();
+        }
+      }
+      catch (Exception ex)
+      {
+        response = new ComplexResponse<StringBuilder>(ex);
+      }
+      return response;
+    }
+  }
+}
