5ffaae04e8dfb33e8388160ce073860c890a7933
DESKTOP-OD9571B\izrra <izrra.ch@icloud.com>
2020-07-02 14:26:48 -0700

    +IEntityQuery

---
+++ Combine.Sdk/Storage/Definitions/DataProvider/Interfaces/IEntityQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Combine.Sdk.Storage.Definitions.DataProvider.Interfaces
+{
+  public interface IEntityQuery<T> : IQueryable where T : class, IEntity, new()
+  {
+  }
+}
